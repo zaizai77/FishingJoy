0a4f92d18bdf61840c68cb72d8dcf04ea2274b8a
zaizai77 <98246281+zaizai77@users.noreply.github.com>
2024-03-02 18:38:22 +0800

    Add files via upload

---
+++ Scripts/HotFixEmpty.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+[Hotfix]
+public class HotFixEmpty : MonoBehaviour
+{
+    [LuaCallCSharp]
+    void Start()
+    {
+        
+    }
+
+    [LuaCallCSharp]
+    void Update()
+    {
+        
+    }
+
+    [LuaCallCSharp]
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    [LuaCallCSharp]
+    private void BehaviourMethod()
+    {
+
+    }
+}
---
+++ Scripts/HotFixScripts.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+using System;
+using System.IO;
+
+public class HotFixScripts : MonoBehaviour
+{
+    LuaEnv env;
+
+    public static Dictionary<string, GameObject> prefabDict = new Dictionary<string, GameObject>();
+
+    private void Awake()
+    {
+        env = new LuaEnv();
+        env.AddLoader(MyLoader);
+        env.DoString("require 'fish'");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private byte[] MyLoader(ref string filePath)
+    {
+        //¾ø¶ÔÂ·¾¶
+        string absPath = @"D:\unity\FishingJoy\UnityPackageManager\" + filePath + ".lua.txt";
+        return System.Text.Encoding.UTF8.GetBytes(File.ReadAllText(absPath));
+    }
+
+    private void OnDisable()
+    {
+        env.DoString("require'fishDispose'");
+    }
+
+    private void OnDestroy()
+    {
+        env.Dispose();
+    }
+
+    [LuaCallCSharp]
+    public static void LoadResource(string resName,string filePath)
+    {
+        AssetBundle ab = AssetBundle.LoadFromFile(@"D:\unity\FishingJoy\AssetBundles\"+filePath);
+        GameObject gameObject = ab.LoadAsset<GameObject>(resName);
+        prefabDict.Add(resName, gameObject);
+    }
+
+    [LuaCallCSharp]
+    public static GameObject GetGameObject(string goName)
+    {
+        return prefabDict[goName];
+    }
+}
